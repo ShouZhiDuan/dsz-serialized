f25f4da692fb4af59df0f130e7a435b295c2898e
段守志 <shouzhi.duan@nvxclouds.com>
2021-07-26 10:16:59 +0800

    初始化项目

---
+++ src/main/java/com/serialized/DszSerializedBakApplication.java
@@ -0,0 +1,13 @@
+package com.serialized;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DszSerializedBakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DszSerializedBakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/serialized/DszSerializedBakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serialized;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DszSerializedBakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
