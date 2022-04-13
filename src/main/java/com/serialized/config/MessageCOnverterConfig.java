22327f3ff330173b4e95ab58921fb0fd52e07314
段守志 <dszazhy1314@163.com>
2022-04-13 12:27:01 +0800

    add resttemplate call

---
+++ src/main/java/com/serialized/config/MessageCOnverterConfig.java
@@ -0,0 +1,29 @@
+package com.serialized.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.cbor.MappingJackson2CborHttpMessageConverter;
+import org.springframework.http.converter.protobuf.ProtobufHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+/**
+ * @Auther: ShouZhi@Duan
+ * @Date: 2022/4/13 10:29
+ * @Description:
+ */
+@Configuration
+public class MessageCOnverterConfig {
+    @Bean
+    public ProtobufHttpMessageConverter protobufHttpMessageConverter() {
+        return new ProtobufHttpMessageConverter();
+    }
+    @Bean
+    public RestTemplate restTemplate(ProtobufHttpMessageConverter protobufHttpMessageConverter) {
+        //MappingJackson2CborHttpMessageConverter mappingJackson2CborHttpMessageConverter = new MappingJackson2CborHttpMessageConverter();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(Arrays.asList(protobufHttpMessageConverter));
+        return restTemplate;
+    }
+}
--- src/test/java/com/serialized/DszSerializedBakApplicationTests.java
+++ src/test/java/com/serialized/DszSerializedBakApplicationTests.java
@@ -1,17 +1,26 @@
 package com.serialized;
 
-import com.serialized.model.OrderProto;
+import com.nvxclouds.medusaskadicommon.proto.Base;
+import com.nvxclouds.medusaskadicommon.proto.DataNodeActivation;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class DszSerializedBakApplicationTests {
 
+    @Autowired
+    RestTemplate restTemplate;
+
+    private final static String URL = "http://192.168.50.177:11443/v2/datanode/activation";
 
     @Test
-    void protoTest() {
-        OrderProto.Order.Builder builder = OrderProto.Order.newBuilder().setAddr("tes addr ").setName("test proto name");
-
+    void callMedusa() {
+        DataNodeActivation.DataNodeActivationRequestDTO.Builder builder = DataNodeActivation.DataNodeActivationRequestDTO.newBuilder();
+        DataNodeActivation.DataNodeActivationRequestDTO requestDTO = builder.setHardwareSignature("test").setDataNodeName("test node name").setUrl("http://192.168.50.180:8080/").build();
+        Base.Result rest = restTemplate.postForObject(URL, requestDTO, Base.Result.class, (Object) null);
+        System.out.println(rest);
     }
 
 }
